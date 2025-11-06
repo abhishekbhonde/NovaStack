0a566b6cabcc1ac06f4e027e024a6d6441d419a9
Abhishek Bhonde <abhishekbhonde567@gmail.com>
2025-11-06 22:34:44 +0530

    feat: add docker file

--- user-service/src/config/db.ts
+++ user-service/src/config/db.ts
@@ -3,7 +3,7 @@ import mongoose  from "mongoose";
 export const connectDB = async()=>{
     try {
         //@ts-ignore
-        await mongoose.connect(process.env.MONGO_URI || "");
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("Database connected successfully");
     } catch (error) {
         console.error("Database connection failed:", error);
